447fb3a08997d9470799536dcabc6da957336edf
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2024-10-03 13:01:27 -0300

    r

--- exercicio 2/index.php
+++ exercicio 2/index.php
@@ -5,29 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        img {
+            width: 300px;
+            height: 300px;
+        }
+    </style>
 </head>
 
 <body>
     <?php
-
-    $texto = '';
-    $url_imagem = '';
-
     $datetime = new DateTime("now", new DateTimeZone("America/Sao_Paulo"));
     $hora = $datetime->format("H");
 
     if (0 <= $hora && $hora <= 12) {
-        $texto = 'Bom dia!';
-        $url_imagem = 'images/ceu-manha.jpg';
+        $result = "<h1>Bom dia!</h1> <img src='images/ceu-manha.jpg' alt=''";
     } elseif ($hora > 12 && $hora < 18) {
-        $texto = "Boa tarde!";
-        $url_imagem = 'images/ceu-tarde.jpg';
+        $result = "<h1>Boa tarde!</h1> <img src='images/ceu-tarde.jpg' alt=''";
     } elseif ($hora >= 18 && $hora <= 24) {
-        $texto = "Boa noite!";
-        $url_imagem = 'images/ceu-noite.jpg';
+        $result = "<h1>Boa noite!</h1> <img src='images/ceu-noite.jpg' alt=''";
     }
 
-    echo "<h1>$texto</h1> <br> <img src='$url_imagem' alt='' width='300px' height='300px'>";
+    echo $result;
 
     ?>
 </body>
